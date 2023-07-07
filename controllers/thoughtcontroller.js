f28c0fac140f126ae24f0c9b035925652ab7dc25
Dior Goolsby <122416848+negromcgee@users.noreply.github.com>
2023-07-07 16:57:42 -0700

    setting up ID for thoughts

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -16,3 +16,39 @@ const thoughtController  = { getAllThoughts(req, res) {
         res.sendStatus(400)
     });
 },
+
+createThought({ params, body }, res) {
+    return Thought.create(body)
+    .then(({ _id}) => {
+        return User.findOneAndUpdate(
+            { username: body.username},
+            { $addToSet: { thoughts: _id }},
+            { new: true}
+        );
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user found with this name'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+    
+},
+
+getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+    .populate({
+        path: 'user',
+        select: '-__v'
+    })
+    
+    .select('-__v')
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+        console.log(err)
+        res.status(400).json(err)
+    });  
+},
+
