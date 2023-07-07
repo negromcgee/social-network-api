5de4046e354253e40db672c876cae616ab9e9be3
Dior Goolsby <122416848+negromcgee@users.noreply.github.com>
2023-07-07 16:52:23 -0700

    setting up thought retrieval

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -2,4 +2,17 @@ const Thought = require('../models/thought');
 const User = require('../models/user');
 const Reaction = require('../models/reaction');
 
-const thoughtController  = {
+const thoughtController  = { getAllThoughts(req, res) {
+    Thought.find({})
+    .populate({
+        path: 'user',
+        select: '-__v'
+    })
+    .select('-__v')
+    .sort ({ _id: -1 })
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(400)
+    });
+},
