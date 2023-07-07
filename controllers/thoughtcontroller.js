aa41251622b1b91ec7554834c27195586fd87f3e
Dior Goolsby <122416848+negromcgee@users.noreply.github.com>
2023-07-07 16:51:28 -0700

    changing constants

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -1,2 +1,5 @@
-const { Thought, User } = require("../models");
-const thoughtController = {
+const Thought = require('../models/thought');
+const User = require('../models/user');
+const Reaction = require('../models/reaction');
+
+const thoughtController  = {
