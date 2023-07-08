204a1691b23b79c56d0805f60b8190a58b1ddef5
Dior Goolsby <122416848+negromcgee@users.noreply.github.com>
2023-07-07 17:01:41 -0700

    adding/deleting reaction function

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -75,3 +75,33 @@ deleteThought ({ params }, res) {
     })
     .catch(err => res.status(400).json(err))
 },
+
+addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true, runValidators: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought with this id" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
+};
+
+module.exports = thoughtController;
+
