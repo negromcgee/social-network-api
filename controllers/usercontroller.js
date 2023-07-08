bbd527630fe06e157e4a38e19528aa9eda259724
Dior Goolsby <122416848+negromcgee@users.noreply.github.com>
2023-07-07 17:47:39 -0700

    adding create/delete user function

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,2 +1,39 @@
 const { User, Thought } = require("../models");
 const userController = {
+    getAllUser(req, res) {
+        User.find({})
+          .populate({
+            path: "friends",
+            select: "-__v",
+          })
+          .select("-__v")
+          .sort({ _id: -1 })
+          .then((dbUserData) => res.json(dbUserData))
+          .catch((err) => {
+            console.log(err);
+            res.sendStatus(400);
+          });
+      },
+
+      createUser({ body }, res) {
+        User.create(body)
+        .then(dbUserData => res.sendStatus(200))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
+    deleteUser ({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err))
+    },
+
+
