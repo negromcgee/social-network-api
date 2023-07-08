8035704312c130c379ff810a70e94cadf4934ac0
Dior Goolsby <122416848+negromcgee@users.noreply.github.com>
2023-07-07 17:57:49 -0700

    adding getuserbyid function, add/delete friend function

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -36,4 +36,64 @@ const userController = {
         .catch(err => res.status(400).json(err))
     },
 
+     updateUser ({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(400).json({ message: 'No users found with this id!'});
+                return;
+            }
+            res.json(dbUserData)
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    
 
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+           .populate({
+               path: 'thoughts',
+               select: '-__v'
+            })
+            .populate ({
+                path: 'friends',
+                select: '-__v'
+            })
+           .select('-__v')
+           .then(dbUserData => res.json(dbUserData))
+           .catch(err => {
+               console.log(err)
+               res.status(400).json(err)
+            
+        });
+        
+    },
+
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId},
+            { $addToSet: { friends: params.friendId }},
+            { new: true }
+        )
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: {friends: params.friendId} },
+            {new: true }
+        )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+    }
+}
+module.exports = userController;
